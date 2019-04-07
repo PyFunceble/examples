534011dbdee07dea79c3d8113f3957acd299ad33
funilrys <contact@funilrys.com>
2019-04-07 17:31:47 +0200

    Introduction of a second example

---
+++ urls_availability.py
@@ -0,0 +1,92 @@
+"""
+The tool to check the availability or syntax of domains, IPv4 or URL.
+
+::
+
+
+    ██████╗ ██╗   ██╗███████╗██╗   ██╗███╗   ██╗ ██████╗███████╗██████╗ ██╗     ███████╗
+    ██╔══██╗╚██╗ ██╔╝██╔════╝██║   ██║████╗  ██║██╔════╝██╔════╝██╔══██╗██║     ██╔════╝
+    ██████╔╝ ╚████╔╝ █████╗  ██║   ██║██╔██╗ ██║██║     █████╗  ██████╔╝██║     █████╗
+    ██╔═══╝   ╚██╔╝  ██╔══╝  ██║   ██║██║╚██╗██║██║     ██╔══╝  ██╔══██╗██║     ██╔══╝
+    ██║        ██║   ██║     ╚██████╔╝██║ ╚████║╚██████╗███████╗██████╔╝███████╗███████╗
+    ╚═╝        ╚═╝   ╚═╝      ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝╚══════╝╚═════╝ ╚══════╝╚══════╝
+
+This is an example which repond to the following problematic(s):
+
+    * How can I get the avaibility of an URL with PyFunceble ?
+
+Author:
+    Nissar Chababy, @funilrys, contactTATAfunilrysTODTODcom
+
+Special thanks:
+    https://pyfunceble.readthedocs.io/en/2.x.x/special-thanks.html
+
+Contributors:
+    http://pyfunceble.readthedocs.io/en/2.x.x/contributors.html
+
+Project link:
+    https://github.com/funilrys/PyFunceble
+
+Project documentation:
+    https://pyfunceble.readthedocs.io/en/2.x.x/
+
+Project homepage:
+    https://funilrys.github.io/PyFunceble/
+
+License:
+::
+
+
+    MIT License
+
+    Copyright (c) 2017, 2018, 2019 Nissar Chababy
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+"""
+
+from PyFunceble import load_config
+from PyFunceble import url_test as PyFunceble
+from PyFunceble.cli_core import CLICore
+
+from helpers import print_result
+
+# We initiate the list of URLs we are going to test.
+URLS = [
+    "https://google.com",
+    "http://tweeetttter.com",
+    "ftp://github.com",
+    "http://examplessss.ooooorgg",
+    "http://twitter.com",
+]
+
+# We load our configuration.
+#
+# Note: We need this to print the logo but if you
+# doesn't need the logo, you can ignore this.
+load_config(generate_directory_structure=False)
+
+# We print the PyFunceble logo.
+CLICore.colorify_logo(home=True)
+
+
+for urls in URLS:
+    # We loop through the list of URLs.
+
+    # And we print the domain and status with the right coloration!
+    print_result(urls, PyFunceble(urls))
